921bb79a64374cfe9820d552f4a45603908c1060
Yehonatan Cohen Scaly <scalys7@gmail.com>
2025-10-21 14:54:06 +0300

    fix gcd_uint to use bingcd

--- src/uint/gcd.rs
+++ src/uint/gcd.rs
@@ -136,11 +136,7 @@ impl<const LIMBS: usize> OddUint<LIMBS> {
     /// Compute the greatest common divisor of `self` and `rhs`.
     #[inline(always)]
     pub const fn gcd_uint(&self, rhs: &Uint<LIMBS>) -> Self {
-        if LIMBS == 1 {
-            Self::classic_bingcd(self, rhs)
-        } else {
-            Self::safegcd(self, rhs)
-        }
+        Self::bingcd(self, rhs)
     }
 
     /// Compute the greatest common divisor of `self` and `rhs`.
@@ -148,11 +144,7 @@ impl<const LIMBS: usize> OddUint<LIMBS> {
     /// Executes in variable time w.r.t. all input parameters.
     #[inline(always)]
     pub const fn gcd_uint_vartime(&self, rhs: &Uint<LIMBS>) -> Self {
-        if LIMBS == 1 {
-            Self::classic_bingcd_vartime(self, rhs)
-        } else {
-            Self::safegcd_vartime(self, rhs)
-        }
+        Self::bingcd_vartime(self, rhs)
     }
 
     /// Compute the greatest common divisor of `self` and `rhs` using the Binary GCD algorithm.
